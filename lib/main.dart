4a54699b375156be36de6b2eeae62cf7460aa564
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-04-08 00:17:45 +0430

    week6 E25 create input layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,46 @@ void main() {
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
 
+  Widget getRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(3),
+          child: Text(
+            '1',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: textGrey),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(3),
+          child: Text(
+            '1',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: textGrey),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(3),
+          child: Text(
+            '1',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: textGrey),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(3),
+          child: Text(
+            '1',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: textGrey),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,9 +66,16 @@ class Application extends StatelessWidget {
               Expanded(
                 flex: 7,
                 child: Container(
-                  height: 200,
-                  color: backgroundGrey,
-                ),
+                    color: backgroundGrey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        getRow(),
+                        getRow(),
+                        getRow(),
+                        getRow(),
+                      ],
+                    )),
               )
             ],
           ),
